d84e82a6d79b91d07019a8387e14e0d4c793251d
xovzd <144159255+2youb00@users.noreply.github.com>
2023-10-15 17:35:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let loaderBar=document.querySelector('.loaderBar');
 let body=document.querySelector('body');
 let discriptoin=document.querySelector('.discriptoin');
 let popup=document.querySelector('.modal');
+let clickaudio=document.getElementById('clickaudio');
+let clickaudio2=document.getElementById('clickaudio2');
+let popupaudio=document.getElementById('popupaudio');
 makebtn.addEventListener('click',()=>{
 loaderBar.classList.remove('display-none');
 makebtn.classList.add('display-none');
@@ -18,13 +21,16 @@ discriptoin.innerHTML= 'Reset all old settings is in progress....'; },4500);
 setTimeout(()=>{
 popup.classList.remove('display-none');
 loaderBar.classList.add('display-none');
+popupaudio.play();
 },6100);
-
+clickaudio.play();
 })
 yesbtn.addEventListener('click',()=>{
     popup.innerHTML='thank you for tring the webside ';
+    clickaudio2.play();
 })
 nobtn.addEventListener('click',()=>{
     popup.innerHTML='i dont give a fuck';
+    clickaudio2.play();
 })
 
